ccf2beb8bd4de20e95e8b7dbe512e9d959771a42
Jeswin Kumar <jeswinpk@agilehead.com>
2019-01-09 15:05:35 +0530

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
 import libpeer = require("libpeer");
+import { Server } from "http";
 
 libpeer.init({
   appId: "abcdefgh",
   masters: ["m1pubkey", "m2pubkey"]
 });
 
-const apiLocations = libpeer.getAPILocations("/posts/latest");
+libpeer
+  .getResources(["/posts/latest", "/blogroll"])
+  .then((locations: any) => {
+    const results = locations
+      .find((x: any) => x.id === "/posts/latest")
+      .fetch();
+    results.then((posts: any) => console.log(posts));
+    results.then(libpeer.registerAsPeer("/posts/latest"));
+  });
 
-
+/* server */
+Server.defineResources([
+  { resource: "/posts/latest", cache: { maxAge: 600 }, handler: (req: any) => {} }
+]);
+libpeerserver.listen("/posts/latest", (req: any) => {
+  req.respond("hello world");
+});
