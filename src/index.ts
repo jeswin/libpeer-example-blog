47186a0a7decd2ef660663d2c0a1a29f863b1f51
Jeswin Kumar <jeswinpk@agilehead.com>
2019-01-17 23:18:59 +0530

    More details

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,67 @@
-import libpeer = require("libpeer");
+import masterNode = require("libpeer-masternode");
+import cacheHandler = require("libpeer-peernode-cache-handler");
 
-const Cache = libpeer.Cache;
-const MasterNode = libpeer.MasterNode;
+const instance = masterNode.createInstance();
 
+/*
+  This is the method called by clients to register itself
+  as a cache provider to other peers.
+
+  Peers will pass two parameters:
+    1. Name of the caching bucket. eg: "latest-posts"
+    2. Any relevant parameters. eg: 100
+    The example above means that the peer can service latest-posts for say user-id 100.
+*/
+masterNode.registerRoute(instance, "/cache", cacheHandler);
+
+/* 
+  Routes 
+  
+  Define all the routes for the application.
+*/
+
+/* Return feed for a user */
 async function readFeedFromDatabase() {
   return await ssb.readStream(id);
 }
 
 async function getFeed(id: string) {
-  const peers = await cache.getFeed({ id });
+  const peers = await cacheGetFeed(id);
   return peers.length ? peers : await readFeedFromDatabase(id);
 }
+
+async function cacheGetFeed(id: string) {
+  return peerCache.getBucket("getFeed").filter((x: any) => x.id === id);
+}
+
+masterNode.registerRoute(instance, "/feed", getFeed);
+masterNode.registerRoute(instance, "/cache/get-feed", cacheGetFeed);
+
+/* Get latest posts for a user */
 
 async function readLatestFromDatabase(id: string, count: number) {
   return [];
 }
 
 async function getLatest(id: string, count: number) {
-  const peersWithLatest = await cache.getLatest({ id, count });
+  const peersWithLatest = await cacheGetLatest(id, count);
   return peersWithLatest.length > 2
     ? peersWithLatest
     : await (async function() {
-        const peersWithFullFeed = await cache.getFeed({ id });
+        const peersWithFullFeed = await cacheGetFeed(id);
         return peersWithLatest.length + peersWithFullFeed.length > 2
           ? peersWithLatest.concat(peersWithFullFeed)
           : await readLatestFromDatabase(id, count);
       })();
 }
+
+async function cacheGetLatest(id: string, count: number) {
+  return peerCache
+    .buckets("getLatest")
+    .filter((x: any) => x.id === id && x.count === count);
+}
+
+masterNode.registerRoute(instance, "/latest", getLatest);
 
 const handlers = [homepageHandler];
 
